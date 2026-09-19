03b83ccdcf86736e1356e424cc607a261825623c
pranavponnada <pranavponnada07@gmail.com>
2026-09-20 01:25:14 +1000

    added bubble sort

---
+++ src/sort/bubble_sort.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+void bubble_sort(int arr[], int n) {
+	for (int i = 0; i < n-1; i++) {
+		for (int j = 0; j < n - 1 - i; j++) {
+			if (arr[j] > arr[j+1]) {
+				int temp;
+				temp = arr[j];
+				arr[j] = arr[j+1];
+				arr[j+1] = temp;
+			}
+		}
+	}
+}
+
+int main () {
+	int n;
+	printf("Enter array size: ");
+	scanf("%d", &n);
+	int arr[n];
+	for (int i = 0; i < n; i++) {
+		printf("Enter array element: ");
+		scanf("%d", &arr[i]);
+	}
+	printf("Array before sorting: ");
+	for (int i = 0; i < n; i++) {
+		printf("%d ", arr[i]);
+	}
+	printf("\n");
+	bubble_sort(arr, n);
+	printf("Array after sorting: ");
+	for (int i = 0; i < n; i++) {
+		printf("%d ", arr[i]);
+	}
+	printf("\n");
+	return 0;
+}
